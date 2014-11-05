a6c27bbc9aac254dbed341c4c6e847180397b205
Sebastian Wizert <sebastianwizert@gmail.com>
2014-11-05 10:27:29 +0000

    Update Player.java

--- Player.java
+++ Player.java
@@ -16,7 +16,11 @@ import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.Timer;
 
-public class Player {
+import com.sebastianwizert.alieninvasion.Sound.Position;
+
+public final class Player {
+	
+	
 	
 	//Class Variables
 	private static final int FRAME_WIDTH = 600;
@@ -26,38 +30,43 @@ public class Player {
 	//Instance variables.
 	private double x;
 	private double y;
+	private double deegre = 90;
 	private int width;
 	private int height;
 	
 	private int speed;
-	private int health = 200;
-	private int lastKnownHealth;
+	private double health = 200;
+	private double lastKnownHealth;
+	private int numOfMissiles = 9999;
+	private int numOfShockWaves = 9999;
 
 	private boolean turnLeft;
 	private boolean turnRight;
 	private boolean forward;
 	private boolean backward;
 	private boolean playerHit;
-	private boolean playersDeath;
-	private int dx = 0;
-	private int dy = 0;
-	private double deegre = 0;
+	private boolean keepShooting;
+	private boolean playerDead;
+
+	private File playerImage = new File("gfx/ufo.png");
+	private File playerImage2 = new File("gfx/ufo2.png");
+	private BufferedImage ufoImage, ufoImage2;
+
+	private ArrayList<Weapon> weapons = new ArrayList<Weapon>();
 	
-	File f = new File("src/ufo.png");
-	File f2 = new File("src/ufo2.png");
-	BufferedImage ufoImage, ufoImage2;
-	
-	private ArrayList<Bullet> bullets = new ArrayList<Bullet>();
 
 	private Timer shakeTimer;
-	int timesShaken;
+	private int timesShaken;
 	
+	private long lastFire, firingInterval = 200;
+	private String currentWeapon;
 	
 	//No args constructor.
-	public Player() {
+	public Player(double newX, double newY) {
 		//Starting position
-		x = 300; 
-		y = 300;
+		this.x = newX;
+		this.y = newY;
+		
 		
 		//Starting width/height.
 		width = 100;
@@ -67,8 +76,8 @@ public class Player {
 		speed = 15; 
 		
 		try{
-			ufoImage = ImageIO.read(f);
-			ufoImage2 = ImageIO.read(f2);
+			ufoImage = ImageIO.read(playerImage);
+			ufoImage2 = ImageIO.read(playerImage2);
 		
 		} catch(Exception e){
 			System.out.println("file corupted");
@@ -78,27 +87,70 @@ public class Player {
 		ActionListener shakerActionListener = new ActionListener() {
 			public void actionPerformed(ActionEvent actionEvent) {
 					timesShaken++;
-					
 					//System.out.println("timesShaken-> "+timesShaken);
 				}
 			};   
 		shakeTimer = new Timer(10, shakerActionListener);
-		
-		
 	}
 	
-	public void shoot(){
-		Bullet b = new Bullet((int)x, (int)y, deegre);
-		bullets.add(b);
-	}
 	
+	public void shoot(String weaponType){
+
+		// check that we have waiting long enough to fire
+		if (System.currentTimeMillis() - lastFire < firingInterval) {
+			return;
+		}
+		// if we waited long enough, create the shot entity, and record the time.
+		lastFire = System.currentTimeMillis();
+
+		if(weaponType.equals("bullet")){
+			if(Setup.soundSwitch == true){
+			 //new Sound("src/laser1.wav", Position.RIGHT).start(); 
+				new Sound2("sfx/laser1.wav");
+			}
+			Weapon b = new Bullet((int)x, (int)y, deegre);
+			weapons.add(b);
+			//System.out.println("players coordinates passed to bullet x "+(int)x + " y "+(int)y+" deegre "+deegre);
+		} else if(weaponType.equals("missile")){
+			if(numOfMissiles > 0) {
+				if(Setup.soundSwitch == true){
+					//new Sound("src/rocket2.wav", Position.RIGHT).start(); 
+					new Sound2("sfx/rocket2.wav");
+				}
+				Weapon m = new Missile((int)x, (int)y, deegre);
+				weapons.add(m);
+				numOfMissiles--;
+			}
+		} else if(weaponType.equals("shockWave")){
+			if(numOfShockWaves > 0) {
+				if(Setup.soundSwitch == true){
+					//new Sound("src/expl1.wav", Position.RIGHT).start();
+					new Sound2("sfx/expl1.wav");
+				}
+				Weapon sw = new ShockWave((int)x, (int)y);
+				weapons.add(sw);
+				numOfShockWaves--;
+			}
+		}
+		
+	}
+
 	/*
 	 * This method will update the position of the object. If they are pressing either the left/right/up/down key, then the shape will move.
 	 * This method is called in the shapeUpdate() method of the ShapeAppPanel class.
 	 */
 	public void update() {
 		
-
+		if(keepShooting == true){
+			shoot(currentWeapon);
+		}
+		
+		if(health < 0){
+			this.playerDead = true;
+		} else {
+			this.playerDead = false;
+		}
+		
 		if(turnLeft) {
 			deegre-=10;
 		}
@@ -116,33 +168,29 @@ public class Player {
 		this.y -=  this.speed * Math.sin(Math.toRadians(this.deegre-90));
 		}
 		
-		
-		if(x < 0) { // If they hit the left border, then set the position of x to 0.
-			x = 0;
+		//Player vs boundries logic
+		//System.out.println("player X " + x + " player Y " + y);
+		if(x < 0) { 
+			x=550;
+		}		
+		if(x + width > FRAME_WIDTH+50) { 
+			x=0;	
 		}
-		
-		if(x + width > FRAME_WIDTH) { //If they hit the right border, then set the position of x to the width of the panel - the width of the object.
-			x = FRAME_WIDTH - width;
-		}
-		
-		//If they hit the top border, then set the position of u to 0.
 		if(y < 0) {
-			y = 0; 
+			y = 500; 
 		}
-		
-		//If they hit the bottom border, then set the position of y to the height of the panel - the height of the object.
-		if(y + height >= FRAME_HEIGHT) {
-			y = FRAME_HEIGHT - height;
+		if(y + height >= FRAME_HEIGHT+50) {
+			y = 0;
 		}
 		
 		//Reset the values to 0.
-		dx = 0;
-		dy = 0;
+		//dx = 0;
+		//dy = 0;
 		
 		//player hit
-		if(lastKnownHealth != this.health) {
+		if(this.lastKnownHealth != this.health) {
 			playerHit = true;
-			lastKnownHealth = this.health;
+			this.lastKnownHealth = this.health;
 		} else if(lastKnownHealth == this.health) {
 			this.playerHit = false;
 		}
@@ -165,6 +213,9 @@ public class Player {
 				y-=0.9;
 			}
 		}
+		
+		//System.out.println("Player coordinates in update before cast x "+x + " y "+y);
+		//System.out.println("Player coordinates in update after cast x "+(int)x + " y "+(int)y);
 	}
 	
 	//Draw the shape of the object.
@@ -215,17 +266,50 @@ public class Player {
 	public double getX() {
 		return this.x;
 	}
+	public void setX(double newX){
+		this.x = newX;
+	}
 	public double getY() {
 		return this.y;
 	}
-	public ArrayList getBullets(){
-		return bullets;
+	public void setY(double newY){
+		this.y = newY;
 	}
-	public int getHealth() {
+	public ArrayList getWeapons(){
+		return weapons;
+	}
+
+	public double getHealth() {
 		return health;
 	}
-	public void setHealth(int health) {
-		this.health = health;
+	public void setHealth(double d) {
+		this.health = d;
+	}
+	public Double getDeegreAngle(){
+		return this.deegre;
+	}
+	public int getNumOfMissiles(){
+		return this.numOfMissiles;
+	}
+	public void setNumOfMissiles(int newNumOfMissiles){
+		this.numOfMissiles = newNumOfMissiles;
+	}
+	public int getNumOfShockWaves(){
+		return this.numOfShockWaves;
+	}
+	public void setNumOfShockWaves(int newNumOfShockWaves) {
+		this.numOfShockWaves = newNumOfShockWaves;
+	}
+	public void setShot(boolean isShoting, String weaponType){
+		this.currentWeapon = weaponType;
+		this.keepShooting = isShoting;
+	}
+
+	public boolean isPlayerDead(){
+		return this.playerDead;
+	}
+	public void setPlayerDead(boolean newPlayerDead){
+		this.playerDead = newPlayerDead;
 	}
 	
 }
