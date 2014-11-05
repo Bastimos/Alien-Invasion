457f012b51c8fcbc6759cbd3defcac56db9578b4
Sebastian Wizert <sebastianwizert@gmail.com>
2014-11-05 10:26:13 +0000

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -6,12 +6,19 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
 
-public class Menu {
+public final class Menu {
 	
-	public Rectangle playButton = new Rectangle(200, 150, 120 ,50);
-	public Rectangle helpButton = new Rectangle(200, 250, 120 ,50);
-	public Rectangle quitButton = new Rectangle(200, 350, 120 ,50);
+	public Rectangle playButton = new Rectangle(250, 125, 120 ,50);
+	public Rectangle helpButton = new Rectangle(250, 200, 120 ,50);
+	public Rectangle setupButton = new Rectangle(250, 275, 120 ,50);
+	public Rectangle statsButton = new Rectangle(250, 350, 120 ,50);
+	public Rectangle quitButton = new Rectangle(250, 425, 120 ,50);
 
+	public Menu(){
+		
+
+	}
+	
 	public void render(Graphics g){
 		Graphics2D g2d=(Graphics2D)g;
 		
@@ -24,9 +31,13 @@ public class Menu {
 		g.setFont(fnt1);
 		g.drawString("PLAY", playButton.x+20, playButton.y+35);
 		g.drawString("HELP", helpButton.x+20, helpButton.y+35);
+		g.drawString("SETUP", setupButton.x+7, setupButton.y+35);
+		g.drawString("STATS", statsButton.x+7, statsButton.y+35);
 		g.drawString("QUIT", quitButton.x+20, quitButton.y+35);
 		g2d.draw(playButton);
 		g2d.draw(helpButton);
+		g2d.draw(setupButton);
+		g2d.draw(statsButton);
 		g2d.draw(quitButton);
 	}
 }
